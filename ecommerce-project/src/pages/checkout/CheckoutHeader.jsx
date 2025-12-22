095479fb873d61bbbae4dad4f487339299b3d320
sanchit <sanchitdubbewar03@gmail.com>
2025-12-22 17:18:44 +0530

    commit

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,21 +1,20 @@
 import './CheckoutHeader.css';
 
-export function CheckoutHeader()
-{
-    return(
-        <>  
+export function CheckoutHeader() {
+    return (
+        <>
             <div className="checkout-header">
                 <div className="header-content">
                     <div className="checkout-header-left-section">
-                        <a href="/">
+                        <Link to="/">
                             <img className="logo" src="images/logo.png" />
                             <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </a>
+                        </Link>
                     </div>
 
                     <div className="checkout-header-middle-section">
-                        Checkout (<a className="return-to-home-link"
-                            href="/">3 items</a>)
+                        Checkout (<Link className="return-to-home-link"
+                            to="/">3 items</Link>)
                     </div>
 
                     <div className="checkout-header-right-section">
