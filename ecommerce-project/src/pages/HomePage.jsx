e5ba3668f5ce638ca243f11b26f46dfd94142dbf
sanchit <sanchitdubbewar03@gmail.com>
2025-12-22 16:27:35 +0530

    Move Chrckout Page into React

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,6 +5,9 @@ export function HomePage()
 {
     return (
         <>
+
+        <title>Ecommerce-project</title>
+
         <div className="header">
     <div className="left-section">
     <a href="index.html" className="header-link">
