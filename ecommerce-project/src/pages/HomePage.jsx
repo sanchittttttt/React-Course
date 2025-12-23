b38e4061078cbc0d0924716692a1378256133dd3
sanchit <sanchitdubbewar03@gmail.com>
2025-12-23 16:35:12 +0530

    fetch data from the backedend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,14 @@ import { products } from '../../starting-code/data/products'
 import { Header } from '../components/Header';
 
 export function HomePage() {
+   
+    fetch('http://localhost:3000/api/products')
+    .then((response)=>{
+        response.json().then(
+            (data)=>{console.log(data);}
+        )
+    })
+   
     return (
         <>
 
