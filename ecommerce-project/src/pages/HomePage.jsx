1f332a0d2e0072e5b809e8c92faf7d5d454c5188
sanchit <sanchitdubbewar03@gmail.com>
2025-12-28 15:08:46 +0530

    generate the HTML for the Delivery options

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect,useState } from 'react';
 import './HomePage.css';
 import { Header } from '../components/Header';
+import { formatMoney } from '../utils/money';
 
 export function HomePage({cart}) {
    
@@ -48,7 +49,7 @@ export function HomePage({cart}) {
                                 </div>
 
                                 <div className="product-price">
-                                    {(product.priceCents / 100).toFixed(2)}
+                                    {formatMoney(product.priceCents)}
                                 </div>
 
                                 <div className="product-quantity-container">
