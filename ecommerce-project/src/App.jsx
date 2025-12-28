c955357ca78e8ec4de46061cf883e84d2ef85ad9
sanchit <sanchitdubbewar03@gmail.com>
2025-12-28 13:41:55 +0530

    lift cart stare up

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Routes , Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
@@ -5,14 +6,25 @@ import './App.css'
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { ErrorPage } from './pages/ErrorPage';
+import { useEffect,useState } from 'react';
 
 function App() {
+   const [cart,setCart] = useState([]);
+
+   useEffect(
+    () => {
+      axios.get('/api/cart-items')
+        .then((response)=>{
+          setCart(response.data);
+      })
+    }
+   )
 
   return (
     <>
       <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route index element={<HomePage cart={cart}/>} />
+          <Route path="/checkout" element={<CheckoutPage cart={cart}/>} />
           <Route path="/orders" element={<OrdersPage />} />
           <Route path="/tracking" element={<TrackingPage />} />
           <Route path="*" element={<ErrorPage />}/> 
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,10 +3,10 @@ import { useEffect,useState } from 'react';
 import './HomePage.css';
 import { Header } from '../components/Header';
 
-export function HomePage() {
+export function HomePage({cart}) {
    
     const [products,setProducts] = useState([]);
-    const [cart,setCart] = useState([]);
+   
 
     useEffect(()=>{
       axios.get('/api/products')
@@ -14,10 +14,6 @@ export function HomePage() {
            setProducts(response.data)
         });
 
-      axios.get('/api/cart-items')
-        .then((response)=>{
-            setCart(response.data);
-      })
     },[]) 
 
    
