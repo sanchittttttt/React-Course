2bc634a1754c85a7481ac91b2a0fd17989623f61
sanchit <sanchitdubbewar03@gmail.com>
2026-01-13 17:52:11 +0530

    create integration test for product component

---
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -0,0 +1,39 @@
+import { it, expect, describe, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';//renders components in a fake page for testing
+import { Product } from './Product';
+
+describe('Product Component', () => {
+    it('displays product details correctly', () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+
+        const loadCart = vi.fn();
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        expect(
+            screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
+        ).toBeInTheDocument(); //check if product name is displayed
+
+        expect(screen.getByText('$10.90')).toBeInTheDocument(); //check if price is displayed
+
+        expect(screen.getByTestId('product-image')).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg'); //check if image is displayed with correct src
+
+        expect(
+            screen.getByTestId('product-rating-stars-image')
+        ).toHaveAttribute('src', 'images/ratings/rating-45.png'); //check if rating stars image is displayed with correct src
+
+        expect(
+            screen.getByText('87')
+        ).toBeInTheDocument(); //check if rating count is displayed
+    });
+});
